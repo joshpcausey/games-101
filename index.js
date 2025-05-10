a5a44570e71f06fee6925b3762278062c7270d82
Joshua Causey <joshpcausey@gmail.com>
2025-05-09 22:05:48 -0500

    have enemies come from all sides and set a smallest size for enemies

--- index.js
+++ index.js
@@ -43,10 +43,33 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, 'blue');
+const player = new Player(x, y, 30, 'yellow');
 
 const projectile = new Projectile(
   canvas.width / 2,
@@ -60,6 +83,35 @@ const projectile = new Projectile(
 );
 
 const projectiles = [projectile];
+const enemies = [];
+
+function spawnEnemies() {
+  setInterval((event) => {
+    const radius = Math.random() * (30 - 4) + 4;
+
+    let x;
+    let y;
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height; // Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+
+    const color = 'green';
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -67,6 +119,10 @@ function animate() {
   player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 }
 
@@ -85,4 +141,6 @@ addEventListener('click', (event) => {
     new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
   );
 });
+
 animate();
+spawnEnemies();
